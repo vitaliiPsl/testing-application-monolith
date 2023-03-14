1a6a1bfdf9d765209ec7fa85437f605e1bdc05d5
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-03-14 20:31:52 +0200

    Create SignIn request and response Dtos

---
+++ src/main/java/com/example/testing/payload/auth/SignInResponseDto.java
@@ -0,0 +1,14 @@
+package com.example.testing.payload.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignInResponseDto {
+    private String token;
+}
