7490c1b8a3730dbbe799ed04858734a6414f91c2
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-03-14 20:13:49 +0200

    Configure model mapper

---
+++ src/main/java/com/example/testing/config/mapper/ModelMapperConfig.java
@@ -0,0 +1,18 @@
+package com.example.testing.config.mapper;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    ModelMapper modelMapper() {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        return mapper;
+    }
+}
