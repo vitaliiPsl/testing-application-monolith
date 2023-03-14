9246a133c13c26bfa34b604478586d83ac106649
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-03-14 20:12:00 +0200

    Create User repository

---
+++ src/main/java/com/example/testing/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.testing.repository;
+
+import com.example.testing.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String email);
+}
