7bc8e2f7b096d717330f9fa737e21a1e1a110c22
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-03-15 17:26:50 +0200

    Add method for users search by id and role

--- src/main/java/com/example/testing/repository/UserRepository.java
+++ src/main/java/com/example/testing/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.example.testing.repository;
 
 import com.example.testing.model.User;
+import com.example.testing.model.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +10,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findByEmail(String email);
+
+    Optional<User> findByIdAndRole(String id, UserRole role);
 }
