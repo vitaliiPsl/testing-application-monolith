9016b57ed60d0107e865573ca17a1def2a4e1e41
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-03-15 10:51:54 +0200

    Override equals and hashcode for User model

--- src/main/java/com/example/testing/model/User.java
+++ src/main/java/com/example/testing/model/User.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.Collection;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -49,6 +50,19 @@ public class User implements UserDetails {
 
     @LastModifiedDate
     private LocalDateTime updatedAt;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email);
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
