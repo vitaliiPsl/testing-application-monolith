cad158115ce9c13c2bf8feecea78ef681c2131ff
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-03-16 21:57:18 +0200

    Create Test repository

---
+++ src/main/java/com/example/testing/repository/TestRepository.java
@@ -0,0 +1,17 @@
+package com.example.testing.repository;
+
+import com.example.testing.model.Subject;
+import com.example.testing.model.test.Test;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface TestRepository extends JpaRepository<Test, String> {
+
+    Optional<Test> findByIdAndSubject(String testId, Subject subject);
+
+    List<Test> findBySubject(Subject subject);
+}
